dabe004c73afe505596a74401cc7c148560614ed
kimraymxnd <rbk0126@gmail.com>
2017-10-18 10:10:08 -0400

    sol

--- 886 - Named Extension Dialing/sol.py
+++ 886 - Named Extension Dialing/sol.py
@@ -2,37 +2,52 @@ from sys import stdin, stdout
 from collections import defaultdict
 
 def main():
-    #  numpad = {'ABC':2, 'DEF':3, 'GHI':4, 'JKL':5, 'MNO':6, 'PQRS':7, 'TUV':8, 'WXYZ':9}
+    #  creating numpad dictionary
     numpad = {'P':7, 'Q':7, 'R':7, 'S':7, 'T':8, 'U':8, 'V':8, 'W':9, 'X':9, 'Y':9, 'Z':9}
     for i in range(65,65+15):
         numpad[chr(i)] = int((i+1)/3)-20
+
     directory = defaultdict(list)
+
+    # set for extensions
     extensionSet = set()
+
+    #reading in input
     for line in stdin:
         line = line.split()
 
+        # input is directory line
         if(len(line) == 3):
             extensionSet.add(line[2])
+            # add numerical first letter of first name
             firstlast = str(numpad[line[0][0]])
+            # add numerical last name
             for ch in line[1]:
-                # print(ch)
                 firstlast += str(numpad[ch.upper()])
+            # map numerical key to extension
             directory[firstlast].append(line[2])
+
+        # input is user query
         else:
             output = []
-            flag = False
             inp = line[0]
+            # query is an extension
             if inp in extensionSet:
-                print(inp)
+                stdout.write("%s\n"%(inp))
             else:
+                # check if we can find query in each key of dir
                 for val in list(directory):
                     if val.find(inp) == 0:
-                        for x in directory[val]:
-                            output.append(x)
+                        # if found, append to output
+                        for i in directory[val]:
+                            output.append(i)
+                # we find nothing
                 if len(output) == 0:
-                    print(0)
+                    stdout.write("0\n")
+                # we have something; join
                 else:
-                    print(" ".join(output))
-            
+                    stdout.write("%s\n" % (" ".join(output)))
+    # print(directory)
+    return 0
 if __name__=="__main__":
     main()
